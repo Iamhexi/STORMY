602c9c25b003e5902831a4dd5d46b94adfbca8cf
Igor Sosnowicz <igor.sosnowicz@gmail.com>
2020-05-17 00:41:04 +0200

    Version 1.4.4 [Hotfix]

--- install/index.php
+++ install/index.php
@@ -3,7 +3,7 @@
 
 require_once "../classes/Installer.php";
 
-if (isset($_POST['dbServer'], $_POST['dbUser'], $_POST['dbName'])
+if (isset($_POST['dbServer'], $_POST['dbUser'], $_POST['dbName']))
    $installer = new Installer($_POST['dbServer'], $_POST['dbUser'], $_POST['dbPassword'], $_POST['dbName']);
 
 else 
